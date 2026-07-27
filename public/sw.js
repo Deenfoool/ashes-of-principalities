275cd146a4184dd333f67aae5b804083ee312a69
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 22:48:24 +0300

    chore: refresh PWA cache for version 0.9

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ashes-shell-v5'
+const CACHE_NAME = 'ashes-shell-v6'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icon.svg', '/maskable-icon.svg']
 
 self.addEventListener('install', (event) => {
