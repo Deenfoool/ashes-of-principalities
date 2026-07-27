22f9846f38b38387961305f0ed25341cddf31161
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 18:15:34 +0300

    feat: enable server journey UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV3'
+import App from './AppV4'
 import './styles.css'
 import './online.css'
+import './online-player.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
