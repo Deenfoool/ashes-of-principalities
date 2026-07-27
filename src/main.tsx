7f6d6fdce396d998a9f51b488a60bcb1045cb3a0
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 18:47:52 +0300

    feat: boot unified server story UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppV4'
+import App from './AppV5'
 import './styles.css'
 import './online.css'
 import './online-player.css'
 import './compat-v04.css'
+import './story-v05.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
