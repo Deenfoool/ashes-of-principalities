bcc32c49740b13e4faa940b1d2eaaea121b72a39
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 19:52:49 +0300

    chore: invalidate the old layered PWA shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ashes-shell-v2'
+const CACHE_NAME = 'ashes-shell-v3'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icon.svg', '/maskable-icon.svg']
 
 self.addEventListener('install', (event) => {
