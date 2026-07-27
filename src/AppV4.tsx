442f68a511f64f5d5227d1b1613d1701efe4af7a
Денис Маняхин <36488826+Deenfoool@users.noreply.github.com>
2026-07-27 18:12:28 +0300

    feat: compose server journey with existing game

---
+++ src/AppV4.tsx
@@ -0,0 +1,11 @@
+import AppV3 from './AppV3'
+import OnlineJourney from './OnlineJourney'
+
+export default function AppV4() {
+  return (
+    <>
+      <AppV3 />
+      <OnlineJourney />
+    </>
+  )
+}
